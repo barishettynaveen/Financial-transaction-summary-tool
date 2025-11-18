d008f8a2e0ae64922d1eaab3bd410fb1c4dd7920
Laxmi Prasanna Podhem <prasannapodhem@gmail.com>
2025-11-18 12:03:32 -0500

    Update test_transactions.py

--- src/test_transactions.py
+++ src/test_transactions.py
@@ -3,13 +3,14 @@ from src.summary_functions import calculate_income_expense
 
 def test_load_data():
     df = load_and_clean_data("financial_transactions.csv")
-    assert 'date' in df.columns
-    assert df['date'].dtype.name in ['datetime64[ns]', 'datetime64[ns, UTC]']
+    assert "date" in df.columns
+    assert df['date'].dtype.name.startswith("datetime")
 
 def test_income_expense():
     df = load_and_clean_data("financial_transactions.csv")
     result = calculate_income_expense(df)
     assert "total_income" in result
     assert "total_expense" in result
+
 
 
