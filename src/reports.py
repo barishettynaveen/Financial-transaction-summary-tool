705bde51b4718041c1d975a74cb4414aab424661
Laxmi Prasanna Podhem <prasannapodhem@gmail.com>
2025-11-18 21:25:47 -0500

    Update reports.py

--- src/reports.py
+++ src/reports.py
@@ -1,7 +1,2 @@
-def monthly_report(df):
-    df['month'] = df['date'].dt.to_period('M').astype(str)
-    return df.groupby(['month', 'type'])['amount'].sum().unstack(fill_value=0)
-
-def top_transactions(df, n=5):
-    return df.sort_values(by='amount', ascending=False).head(n)
+#intial code
 
